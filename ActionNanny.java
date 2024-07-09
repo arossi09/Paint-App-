9d901dfbb2fbb1a355b01789b7ff4ce4ff496b89
arossi09 <arossi09@calpoly.edu>
2024-07-08 20:10:23 -0700

    Made ActionNanny more readable with switch cases and select to change color functionable

--- ActionNanny.java
+++ ActionNanny.java
@@ -21,58 +21,96 @@ public class ActionNanny implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() instanceof JMenuItem) {
 			JMenuItem menuItem = (JMenuItem) e.getSource();
-			if (menuItem.getActionCommand().equals("Black")) {
-				Officer.setColor(Color.BLACK);
-			} else if (menuItem.getActionCommand().equals("Red")) {
-				Officer.setColor(Color.RED);
-			} else if (menuItem.getActionCommand().equals("Blue")) {
-				Officer.setColor(Color.BLUE);
-			} else if (menuItem.getActionCommand().equals("Green")) {
-				Officer.setColor(Color.GREEN);
-			} else if (menuItem.getActionCommand().equals("Yellow")) {
-				Officer.setColor(Color.YELLOW);
-			} else if (menuItem.getActionCommand().equals("Orange")) {
-				Officer.setColor(Color.ORANGE);
-			} else if (menuItem.getActionCommand().equals("Pink")) {
-				Officer.setColor(Color.PINK);
-			} else if (menuItem.getActionCommand().equals("undo")) {
-				System.out.println("undo");
-				Officer.undo();
-			} else if (menuItem.getActionCommand().equals("erase")) {
-				System.out.println("erase");
-				Officer.setErased(true);
-				Officer.erase();
-			} else if (menuItem.getActionCommand().equals("redo")) {
-				System.out.println("redo");
-				Officer.redo();
-			}else if(menuItem.getActionCommand().equals("copy")){
-				Officer.Copy();
-			}else if(menuItem.getActionCommand().equals("paste")) {
-				Officer.Paste();
-				Officer.tellYourBoss();
-			}else if(menuItem.getActionCommand().equals("save")) {
-				try {
-					Officer.saveShapesToFile();
-				} catch (IOException ex) {
-					throw new RuntimeException(ex);
-				}
-			}else if(menuItem.getActionCommand().equals("load")) {
-				try {
-					Officer.loadShapesFromFile();
+			String actionCommand = menuItem.getActionCommand();
+
+			switch (actionCommand) {
+				case "Black":
+				case "Red":
+				case "Blue":
+				case "Green":
+				case "Yellow":
+				case "Orange":
+				case "Pink":
+					setColorForShape(actionCommand);
+					break;
+				case "undo":
+					Officer.undo();
+					break;
+				case "erase":
+					Officer.setErased(true);
+					Officer.erase();
+					break;
+				case "redo":
+					Officer.redo();
+					break;
+				case "copy":
+					Officer.Copy();
+					break;
+				case "paste":
+					Officer.Paste();
 					Officer.tellYourBoss();
-				} catch (IOException | ClassNotFoundException ex) {
-					throw new RuntimeException(ex);
-				}
-			}else if(menuItem.getActionCommand().equals("new")){
-				Officer.erase();
-				Officer.clearShapes();
-				Officer.clearDeletedShapes();
-			}else{
-				System.out.println(e.getActionCommand());
-				Officer.setShape(e.getActionCommand());
+					break;
+				case "save":
+					try {
+						Officer.saveShapesToFile();
+					} catch (IOException ex) {
+						throw new RuntimeException(ex);
+					}
+					break;
+				case "load":
+					try {
+						Officer.loadShapesFromFile();
+						Officer.tellYourBoss();
+					} catch (IOException | ClassNotFoundException ex) {
+						throw new RuntimeException(ex);
+					}
+					break;
+				case "new":
+					Officer.erase();
+					Officer.clearShapes();
+					Officer.clearDeletedShapes();
+					break;
+				default:
+					System.out.println(actionCommand);
+					Officer.setShape(actionCommand);
+					break;
 			}
 		}
 	}
 
+	private void setColorForShape(String colorCommand) {
+		Color color;
+		switch (colorCommand) {
+			case "Black":
+				color = Color.BLACK;
+				break;
+			case "Red":
+				color = Color.RED;
+				break;
+			case "Blue":
+				color = Color.BLUE;
+				break;
+			case "Green":
+				color = Color.GREEN;
+				break;
+			case "Yellow":
+				color = Color.YELLOW;
+				break;
+			case "Orange":
+				color = Color.ORANGE;
+				break;
+			case "Pink":
+				color = Color.PINK;
+				break;
+			default:
+				return;
+		}
 
+		if (Officer.getSelectedShape() == null) {
+			Officer.setColor(color);
+		} else {
+			Officer.getSelectedShape().setColor(color);
+			Officer.tellYourBoss();
+		}
+	}
 }
